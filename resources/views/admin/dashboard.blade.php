901199de81d02d8c09ef4756bb31e93e99ea2fb1
Azhar Raihan <azharraihan6969@gmail.com>
2022-03-26 01:31:43 +0600

    Setting page done

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,7 +9,7 @@
     <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
-        <div class="row mb-2">
+        <div class="row">
           <div class="col-sm-6">
             <h1 class="m-0 text-dark">Dashboard v2</h1>
           </div><!-- /.col -->
